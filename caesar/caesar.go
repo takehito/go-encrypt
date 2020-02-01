3a112950cbd98e3029f92f77ea63fbd1cda5bac7
takehito <utisama.produce@gmail.com>
2020-02-02 00:22:23 +0900

    ２５以上の値が与えられた時うまく動かなかったので修正

--- caesar/caesar.go
+++ caesar/caesar.go
@@ -1,21 +1,33 @@
 package caesar
 
+var alphabets = "abcdefghijklmnopqrstuvwxyz"
+
 func KeyGen(n int) int {
 	return n
 }
 
 func Encrypt(m string, n int) string {
 	result := make([]byte, len(m))
-	for i, b := range []byte(m) {
-		result[i] = b + byte(n)
+	for i, c := range m {
+		for j, alphabet := range alphabets {
+			if c != alphabet {
+				continue
+			}
+
+			shift := j + n
+			if shift > 25 {
+				result[i] = alphabets[shift-26]
+				continue
+			}
+
+			result[i] = alphabets[shift]
+		}
 	}
+
 	return string(result)
 }
 
 func Decrypt(c string, n int) string {
-	result := make([]byte, len(c))
-	for i, b := range []byte(c) {
-		result[i] = b - byte(n)
-	}
-	return string(result)
+	shift := 26 - n
+	return Encrypt(c, shift)
 }
